14eda7f457077a17ff2c9e50c19b5f0cbc8bbd27
Cyril Fluck <github@fluck.fr>
2022-04-20 17:25:30 -0700

    tmp

--- background.js
+++ background.js
@@ -11,3 +11,32 @@ chrome.runtime.onInstalled.addListener(function() {
         }]);
     });
 });
+
+const baseUrl = 'https://secure.splitwise.com/oauth/authorize';
+const urlParams = {
+  'redirect_uri' : chrome.identity.getRedirectURL(),
+  'response_type' : 'code',
+  'client_id' : 'dCSHbJPoQYPcJ7JA2cSiyf9eJsJFoyNAW7BnIdBC',
+//   'code_challenge': 'MChCW5vD-3h03HMGFZYskOSTir7II_MMTb8a9rJNhnI',
+//   'code_challenge_method': 'S256',
+  'state': 'some random string to protect against cross-site request forgery attacks'
+//   'scope' : 'https://mail.google.com/ https://www.google.com/m8/feeds/',
+//   'include_granted_scopes' : 'true'
+};
+
+const providerDetails = {
+  url : baseUrl + '?' + new URLSearchParams(urlParams).toString(),
+  interactive : true
+}
+
+const callback = (token) => {
+    console.log('callback', token);
+    chrome.identity.clearAllCachedAuthTokens(() => {});
+}
+
+chrome.runtime.onMessage.addListener(() => {
+    console.log('login', providerDetails, urlParams);
+    chrome.identity.launchWebAuthFlow(providerDetails, callback);
+  
+    return true;
+  });
--- query.js
+++ query.js
@@ -1,2 +1,8 @@
 'use strict';
 
+
+const login = () => {
+    chrome.runtime.sendMessage({ login: "true" });
+}
+
+document.getElementById('login').addEventListener('click', login);
